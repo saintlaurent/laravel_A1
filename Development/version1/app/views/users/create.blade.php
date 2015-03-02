a7e92576acee3180b9bd56425140669ba93c13b8
Cat <catherineyangli@outlook.com>
2015-03-02 15:18:23 -0800

    Added "confirm password"

--- Development/version1/app/views/users/create.blade.php
+++ Development/version1/app/views/users/create.blade.php
@@ -10,11 +10,17 @@
             {{Form::label('email', 'Email (will be your username): ')}}
             {{Form::text('email')}}
         </div>
-
+        
         <div>
             {{Form::label('password', 'Password: ')}}
             {{Form::password('password')}}
         </div>
+
+        <div>
+            {{Form::label('confirm_password', 'Confirm password: ')}}
+            {{Form::password('confirm_password')}}
+        </div>
+
           <div>
             {{Form::submit('Create Account')}}
         </div>
